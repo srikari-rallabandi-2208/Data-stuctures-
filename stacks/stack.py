5329603b2e69cdf6506e2562f208acd3a7e6e76b
srikari-rallabandi-2208 <sreeku.ralla@gmail.com>
2022-01-08 06:45:35 +0530

    Added Stack Class

--- stacks/stack.py
+++ stacks/stack.py
@@ -2,33 +2,75 @@
 
 class Node:
     
-    def __init__(self,data,next):
+    def __init__(self,data):
         self.data = data
-        self.next = next
-    
-    @property    
-    def data(self):
-        return self.data
-    
-    @property
-    def data(self,data):
-        self.data = data
-        
-    @property
-    def next(self):
-        return self.next
-    
-    @property
-    def data(self,next):
-        self.next = next
-        
-
+        self.next = None
 class Stack:
     
-    length = 0
-    head = None
-    tail = None
+    size = 0
     
-    def __init__(self,data):
+    def __init__(self):
+        self.top = None
+    
+    def push(self,data):
         node = Node(data)
+        if self.top is None:
+            self.top = node
+        else:
+            temp = self.top
+            node.next = temp
+            self.top = node
+        self.size += 1
     
+    def pop(self):
+        if self.size > 0  and self.top is not None:
+            temp = self.top
+            self.top = self.top.next
+            self.size -= 1
+            return temp.data
+        else:
+            raise Exception("Popping from Empty Stack!!!")
+    
+    def isEmpty(self):
+        return self.size == 0
+    
+    def peek(self):
+        if not self.isEmpty():
+            return self.top.data
+        else:
+            raise Exception("Empty Stack")
+        
+    def __str__(self):
+        out = ""
+        if self.size == 0 or self.top is None:
+            out = "Empty Stack"
+        else:
+            temp = self.top
+            while temp:
+                out+= str(temp.data) + "=>"
+                temp = temp.next
+            out =  out[:-2]   
+        return out
+                
+if __name__ == "__main__":
+    stack = Stack()
+
+    for i in range(11):
+        stack.push(i)
+    
+    print(f"Stack : {stack}") 
+    val = stack.peek()
+    print(f"Peeking: {val}")      
+        
+    for _ in range(1,12):
+        val = stack.pop()
+        print(f"Removed: {val}")
+    
+    print(f"Stack : {stack}")  
+    
+    try:
+        val = stack.peek()
+        print(f"Peeking: {val}")
+    except:
+        print("Caught exception trying to peek")
+     
