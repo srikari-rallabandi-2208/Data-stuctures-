6430b1ec26d304c615cda4d55889dda05261f89c
srikari-rallabandi-2208 <sreeku.ralla@gmail.com>
2022-01-29 14:11:23 +0530

    Given 2 arrays give the common elements in the arrays.

---
+++ arrays/check_common_elements.py
@@ -0,0 +1,15 @@
+def common_elements(list1, list2):
+    p1 = 0
+    p2 = 0
+    result = []
+    while p1 < len(list1) and p2 < len(list2):
+        if list1[p1] == list2[p2]:
+            result.append(list1[p1])
+            p1 += 1
+            p2 += 1
+        elif list1[p1] > list2[p2]:
+            p2 += 1
+        else:
+            p1 += 1
+    return result
+
