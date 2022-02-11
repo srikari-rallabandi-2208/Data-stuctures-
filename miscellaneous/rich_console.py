8045e074810adee11f2b7b14a5abef531fb18526
srikari-rallabandi-2208 <sreeku.ralla@gmail.com>
2022-02-11 18:39:15 +0530

    rich console

---
+++ miscellaneous/rich_console.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from rich.color import Color
+from rich.console import Console, ConsoleOptions, RenderResult
+from rich.table import Table
+
+@dataclass
+class Pet:
+    width: int = 80
+    background_color: Color = Color.from_rgb(255, 255, 255)
+    def __init__(self, name=None, breed=None, age=0):
+        self.name = name
+        self.breed = breed
+        self.age = age
+    
+    def __rich_console__(self, console: Console, options: ConsoleOptions) -> RenderResult:
+        yield f"[b]Pet:[/b] #{self.name}"
+        pet_table = Table("Attribute", "Value")
+        pet_table.add_row("Breed", self.breed)
+        pet_table.add_row("age", str(self.age))
+        yield pet_table
+    
+    def __rich__(self) -> str:
+        return "[bold cyan]"+f"{self.name} is a {self.breed} and is {self.age} years old."
+    
+    def __str__(self):
+        return f"{self.name} is a {self.breed} and is {self.age} years old."
+
+if __name__ == '__main__':
+    pet = Pet("Fido", "Labrador", 3)
+    print(pet)
+    console = Console()
+    console.log(pet)
+    console.print(pet)
