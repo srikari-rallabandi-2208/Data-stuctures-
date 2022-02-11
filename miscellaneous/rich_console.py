a3b62121944ec7ff091af290ba708b889c4575c8
srikari-rallabandi-2208 <sreeku.ralla@gmail.com>
2022-02-11 19:16:47 +0530

    Modified Table Properties

--- miscellaneous/rich_console.py
+++ miscellaneous/rich_console.py
@@ -14,20 +14,22 @@ class Pet:
     
     def __rich_console__(self, console: Console, options: ConsoleOptions) -> RenderResult:
         yield f"[b]Pet:[/b] #{self.name}"
-        pet_table = Table("Attribute", "Value")
+        pet_table = Table(title="Pets")
+        pet_table.add_column("Attribute",justify="left", style="cyan", no_wrap=True)
+        pet_table.add_column("Value",style="magenta")
         pet_table.add_row("Breed", self.breed)
         pet_table.add_row("age", str(self.age))
         yield pet_table
     
-    def __rich__(self) -> str:
-        return "[bold cyan]"+f"{self.name} is a {self.breed} and is {self.age} years old."
+    #def __rich__(self) -> str:
+    #    return "[bold cyan]"+f"{self.name} is a {self.breed} and is {self.age} years old."
     
     def __str__(self):
         return f"{self.name} is a {self.breed} and is {self.age} years old."
 
 if __name__ == '__main__':
     pet = Pet("Fido", "Labrador", 3)
-    print(pet)
+    #print(pet)
     console = Console()
-    console.log(pet)
+    #console.log(pet)
     console.print(pet)
