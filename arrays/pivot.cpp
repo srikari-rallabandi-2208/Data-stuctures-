610313653bf026c323a631bdc272abd1edbcdf75
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-03-26 20:24:40 +0530

    Add files via upload

---
+++ arrays/pivot.cpp
@@ -0,0 +1,17 @@
+//leetcode problem 724 - find pivot index
+
+int pivotIndex(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        int lsum=0,rsum=sum;
+        for(int i=0;i<nums.size();i++){
+            rsum=rsum-nums[i];
+            if(rsum==lsum){
+                return i;
+            }
+            lsum+=nums[i];
+        }
+         return -1;
+    }
