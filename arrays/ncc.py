cd66f0832b72636315ae2f0d65a882a3a6d1ba57
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-03-29 21:42:21 +0530

    Add files via upload

---
+++ arrays/ncc.py
@@ -0,0 +1,12 @@
+'''
+AlgoExpert - Non constructible change - arrays
+'''
+
+def nonConstructibleChange(coins):
+	coins.sort()
+	current_change = 0
+	for coin in coins:
+		if coin > current_change + 1:
+			return current_change + 1
+		current_change += coin
+    return current_change + 1
