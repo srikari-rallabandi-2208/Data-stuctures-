fd838cc2cdb05372e05c805df790cd554aa9e8ed
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-03-30 21:52:43 +0530

    Add files via upload

---
+++ trees/branch_sums.cpp
@@ -0,0 +1,37 @@
+//AlgoExpert - Branch Sums - Binary Trees
+
+#include<vector>
+
+using namespace std;
+
+class BinaryTree{
+	public:
+		int value;
+		BinaryTree *left;
+		BinaryTree *right;
+	BinaryTree(int value){
+		this -> value = value;
+		left = nullptr;
+		right = nullptr;
+	}
+};
+
+void calculateBranchSums(BinaryTree *node, int runningSum, vector<int> &sums);
+
+vector<int> branchSums(BinaryTree *root){
+	vector<int> sums;
+	calculateBranchSums(root, 0, sums);
+	return sums;
+}
+
+void calculateBranchSums(BinaryTree *node, int runningSum, vector<int>&sums){
+	if (node == nullptr)
+		return;
+	int newRunningSum = runningSum + node -> value;
+	if(node -> left == nullptr && node -> right == nullptr){
+		sums.push_back(newRunningSum);
+		return;
+	}
+	calculateBranchSums(node -> left, newRunningSum, sums);
+	calculateBranchSums(node -> right, newRunningSum, sums);
+}
