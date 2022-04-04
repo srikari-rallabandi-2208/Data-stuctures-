33f72f392bb2b411edb9475a6326d6ca60e4d497
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-04-04 21:34:42 +0530

    Add files via upload

---
+++ searchnsort/bubble_sort.py
@@ -0,0 +1,18 @@
+'''
+AlgoExpert - Bubble Sort - Search and Sort
+'''
+
+def bubbleSort(array):
+	is_sorted = False
+	counter = 0
+	while not is_sorted:
+		is_sorted = True
+		for i in range(len(array) - 1 - counter):
+			if array[i] > array[i + 1]:
+				swap(i, i + 1, array)
+				is_sorted = False
+		counter += 1
+	return array
+
+def swap(i, j, array):
+	array[i], array[j] = array[j], array[i]
