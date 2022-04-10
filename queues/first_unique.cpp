7a6061dfec73f5a6d9279fdc3fe846d2d326297c
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-04-10 17:16:36 +0530

    Add files via upload

---
+++ queues/first_unique.cpp
@@ -0,0 +1,16 @@
+//LeetCode - problem 387 - First Unique Character in a String
+
+int firstUniqChar(string s) {
+        unordered_map<char, pair<int, int>> m;
+        int idx = s.size();
+        for (int i = 0; i < s.size(); i++) {
+            m[s[i]].first++;
+            m[s[i]].second = i;
+        }
+        for (const auto& [c, p] : m) {
+            if (p.first == 1) {
+                idx = min(idx, p.second);
+            }
+        }
+        return idx == s.size() ? -1 : idx;
+    }
