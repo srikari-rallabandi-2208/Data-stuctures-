501c860603eb603c053a55643c88a0a803a1a373
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-04-11 21:19:56 +0530

    Add files via upload

---
+++ stacks/rdl.cpp
@@ -0,0 +1,20 @@
+//LeetCode - problem 316 - Remove Duplicate Letters
+
+string removeDuplicateLetters(string s) {
+    vector<int> cand(256, 0);
+    vector<bool> visited(256, false);
+    for (char c : s)
+        cand[c]++;
+    string result = "0";
+    for (char c : s) {
+        cand[c]--;
+        if (visited[c]) continue;
+        while (c < result.back() && cand[result.back()]) {
+            visited[result.back()] = false;
+            result.pop_back();
+        }
+        result += c;
+        visited[c] = true;
+    }
+    return result.substr(1);
+}
