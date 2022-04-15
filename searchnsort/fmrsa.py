1ec0508e34523d099b9cf423fda4ef0c23722847
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-04-15 19:23:28 +0530

    Add files via upload

---
+++ searchnsort/fmrsa.py
@@ -0,0 +1,13 @@
+'''
+LeetCode - problem 154
+'''
+
+def findMin(self, nums):
+    lo, hi = 0, len(nums) - 1
+    while lo < hi:
+        mid = lo + (hi -lo) / 2
+        if nums[mid] > nums[hi]:
+            lo = mid + 1
+        else:
+            hi = mid if nums[hi] != nums[mid] else hi - 1
+    return nums[lo]
