6ad36d91d6f3f24052f33886d32870b772101952
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-04-18 11:00:35 +0530

    Add files via upload

---
+++ miscellaneous/regular_strings.cpp
@@ -0,0 +1,18 @@
+// LeetCode - problem 10 - Regular Expression Matching 
+
+bool match(int s_idx, int p_idx) {
+        return (ss[s_idx] == pp[p_idx]) || (pp[p_idx] == '.' && ss[s_idx]);
+    }
+    
+    bool rec(int s_idx, int p_idx) {
+        if (!pp[p_idx]) return !ss[s_idx];
+        if (pp[p_idx+1] == '*') return rec(s_idx, p_idx+2) || match(s_idx, p_idx) && rec(s_idx+1, p_idx);
+        if (pp[p_idx] == '.') return ss[s_idx] && rec(s_idx+1, p_idx+1);
+        return match(s_idx, p_idx) && rec(s_idx+1, p_idx+1);
+    }
+    
+    bool isMatch(string s, string p) {
+        ss = s, pp = p;
+        return rec(0, 0);
+        
+    }
