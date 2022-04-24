a1d22aa058a9d070074783ff992116ff8428b391
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-04-25 00:09:30 +0530

    Add files via upload

---
+++ arrays/remove_element.py
@@ -0,0 +1,14 @@
+'''
+LeetCode - problem 27 - Remove Element
+'''
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i=0
+        for n in nums:
+            if n!=val:
+                nums[i]=n
+                i+=1
+        return i
+
+        
