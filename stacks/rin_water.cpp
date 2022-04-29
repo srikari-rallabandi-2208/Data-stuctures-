0573bf5cd91e3149250644eac411221f4882ab80
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-04-30 00:53:27 +0530

    Add files via upload

---
+++ stacks/rin_water.cpp
@@ -0,0 +1,12 @@
+//LeetCode - problem 42
+
+int trap(vector<int>& height) {
+    auto l = height.begin(), r = height.end() - 1;
+    int level = 0, water = 0;
+    while (l != r + 1) {
+        int lower = *l < *r ? *l++ : *r--;
+        level = max(level, lower);
+        water += level - lower;
+    }
+    return water;
+}
