9a0f4945bb92bb3e2deaf686e16dc72a581d6d78
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-04-29 18:26:57 +0530

    Add files via upload

---
+++ queues/cards_increasing.py
@@ -0,0 +1,10 @@
+'''
+LeetCode - problem 950
+'''
+
+def deckRevealedIncreasing(self, deck):
+    ind = list(range(len(deck)))
+    for num in sorted(deck):
+        deck[ind[0]] = num
+        ind = ind[2:] + [ind[1]] if len(ind) > 1 else []
+    return deck
