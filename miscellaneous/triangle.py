871b6eb6a1f69279259ebe6a2d288d38a6727782
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-05-01 02:06:11 +0530

    Add files via upload

---
+++ miscellaneous/triangle.py
@@ -0,0 +1,12 @@
+'''
+LeetCode - problem 120 - DP
+'''
+
+def minimumTotal(self, triangle):
+    if not triangle:
+        return 
+    res = triangle[-1]
+    for i in xrange(len(triangle)-2, -1, -1):
+        for j in xrange(len(triangle[i])):
+            res[j] = min(res[j], res[j+1]) + triangle[i][j]
+    return res[0]
