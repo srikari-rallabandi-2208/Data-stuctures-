d430af5dd2c45fedca9569b5fe5dac505d571c18
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-05-06 12:12:39 +0530

    Add files via upload

---
+++ searchnsort/envelopes.cpp
@@ -0,0 +1,12 @@
+//LeetCode - problem 354
+
+int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+        if (envelopes.empty()) return 0;
+        sort(envelopes.begin(), envelopes.end());
+        vector<int> dp(envelopes.size(), 1);
+        for (int i = 0; i < envelopes.size(); ++i)
+            for (int j = 0; j < i; ++j)
+                if (envelopes[j].first < envelopes[i].first && envelopes[j].second < envelopes[i].second)
+                    dp[i]  = max(dp[i] , dp[j] + 1);
+        return *max_element(dp.begin(), dp.end());
+    }
