c4cac95ea547338be3ead4df16d6697fb55b60b1
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-05-19 13:27:02 +0530

    Add files via upload

---
+++ miscellaneous/edit_distance.py
@@ -0,0 +1,19 @@
+'''
+LeetCode - problem 72 - Dynamic Programming
+'''
+
+    def minDistance(self, word1, word2):
+        """Naive recursive solution"""
+        if not word1 and not word2:
+            return 0
+        if not word1:
+            return len(word2)
+        if not word2:
+            return len(word1)
+        if word1[0] == word2[0]:
+            return self.minDistance(word1[1:], word2[1:])
+        insert = 1 + self.minDistance(word1, word2[1:])
+        delete = 1 + self.minDistance(word1[1:], word2)
+        replace = 1 + self.minDistance(word1[1:], word2[1:])
+        return min(insert, replace, delete)
+
