93b31e75e51a2f7c463f914f18eb53a13b8556df
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-05-20 21:22:26 +0530

    Add files via upload

---
+++ miscellaneous/letter_combinations.py
@@ -0,0 +1,17 @@
+'''
+LeetCode - problem 17 - strings
+'''
+
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(nums, [], res)
+        return res
+    
+    def dfs(self, nums, path, res):
+        res.append(path)
+        for i in range(len(nums)):
+            self.dfs(nums[i+1:], path + [nums[i]], res)
