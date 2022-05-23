277c58bbc6212b55d95b51f815bc5d5021a6ad73
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-05-23 23:18:17 +0530

    Add files via upload

---
+++ miscellaneous/jump_game.cpp
@@ -0,0 +1,8 @@
+//LeetCode - problem 55 - DP
+
+bool canJump(int A[], int n) {
+    int i = 0;
+    for (int reach = 0; i < n && i <= reach; ++i)
+        reach = max(i + A[i], reach);
+    return i == n;
+}
