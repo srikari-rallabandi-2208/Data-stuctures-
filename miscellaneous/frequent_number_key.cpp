7cef8ba60ae148d2af84beeb91a053aecb2a7aff
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-05-27 18:45:37 +0530

    Add files via upload

---
+++ miscellaneous/frequent_number_key.cpp
@@ -0,0 +1,9 @@
+//LeetCode - problem 2190 - Hashing 
+
+int mostFrequent(vector<int>& nums, int key) {
+    int cnt[1001] = {}, res = 0;
+    for (int i = 1; i < nums.size(); ++i)
+        if (nums[i - 1] == key && ++cnt[nums[i]] > cnt[res])
+            res = nums[i];
+    return res;
+}
