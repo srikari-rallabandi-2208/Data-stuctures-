af5dcb5d3b8f37a8e71258c1a427c27ada78c943
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-05-27 18:45:57 +0530

    Add files via upload

---
+++ miscellaneous/frequent_number_key.py
@@ -0,0 +1,10 @@
+'''
+LeetCode - problem 2190 - Hashing
+'''
+
+    def mostFrequent(self, nums: List[int], key: int) -> int:
+        c = Counter()
+        for i, n in enumerate(nums):
+            if n == key and i + 1 < len(nums):
+                c[nums[i + 1]] += 1
+        return c.most_common(1)[0][0]
