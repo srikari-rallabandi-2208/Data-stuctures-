7db2024609a1d0872d65105edc671d0da4bdd1ee
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-05-28 15:38:29 +0530

    Add files via upload

---
+++ miscellaneous/ltcd_2264.cpp
@@ -0,0 +1,9 @@
+//LeetCode - problem 2264 - strings
+
+string largestGoodInteger(string num) {
+    char res = 0;
+    for(int i = 2; i < num.size(); ++i)
+        if (num[i] == num[i - 1] && num[i] == num[i - 2])
+            res = max(res, num[i]);
+    return res == 0 ? "" : string(3, res);
+}
