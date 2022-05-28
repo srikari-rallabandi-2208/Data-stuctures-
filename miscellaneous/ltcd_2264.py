c9802c0cc4d7592f1a9cfa02ade8baf6a07828a7
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-05-28 15:38:47 +0530

    Add files via upload

---
+++ miscellaneous/ltcd_2264.py
@@ -0,0 +1,6 @@
+'''
+LeetCode - problem 2264 - strings
+'''
+
+def largestGoodInteger(self, n: str) -> str:
+        return max(n[i-2:i+1] if n[i] == n[i - 1] == n[i - 2] else "" for i in range(2, len(n)))
