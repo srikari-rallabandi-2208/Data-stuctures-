8cb5d08e4ab7da9741016953ddf44312554027e7
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-06-01 23:10:10 +0530

    Add files via upload

---
+++ graphs/course_schedule_2.py
@@ -0,0 +1,21 @@
+'''
+LeetCode - problem 210
+'''
+
+    def findOrder(self, numCourses, prerequisites):
+        indegree = [set() for _ in range(numCourses)]
+        outdegree = [[] for _ in range(numCourses)]
+        for p in prerequisites:
+            indegree[p[0]].add(p[1])
+            outdegree[p[1]].append(p[0])
+        ret, start = [], [i for i in range(numCourses) if not indegree[i]]
+        while start: 
+            newStart = [] 
+            for i in start:
+                ret.append(i)
+                for j in outdegree[i]:
+                    indegree[j].remove(i)
+                    if not indegree[j]:
+                        newStart.append(j)
+            start = newStart 
+        return ret if len(ret) == numCourses else [] 
