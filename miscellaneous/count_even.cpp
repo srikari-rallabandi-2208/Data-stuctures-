3ef70610336e5a1122b3ac94af43c4c3a3ac4f92
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-06-03 18:46:57 +0530

    Add files via upload

---
+++ miscellaneous/count_even.cpp
@@ -0,0 +1,11 @@
+//LeetCode - problem 2180
+
+    int countEven(int num) {
+        int temp = num, sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum % 2 == 0 ? temp / 2 : (temp - 1) / 2;
+    }
+
