5cfc9967617f516f75a293f4f05856c21c8ffa32
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-06-12 00:28:39 +0530

    Update 3sum_closest.cpp

--- arrays/3sum_closest.cpp
+++ arrays/3sum_closest.cpp
@@ -1,6 +1,6 @@
 //LeetCode - problem 16
 
-int threeSumClosest(vector<int>& nums, int target) {
+int threeSumClosest(vector<int>& nums, long int target) {
     if(nums.size() < 3) return 0;
     int closest = nums[0]+nums[1]+nums[2];
     sort(nums.begin(), nums.end());
