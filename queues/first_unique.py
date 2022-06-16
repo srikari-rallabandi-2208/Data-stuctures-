5b4fbfd2345a8070dc4c5b44e2cff9d8aebabfad
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-06-16 18:30:31 +0530

    Update first_unique.py

--- queues/first_unique.py
+++ queues/first_unique.py
@@ -3,6 +3,6 @@ LeetCode - problem 387 - First Unique Character in a String
 '''
 
 def firstUniqChar(self, s):        
-        letters='abcdefghijklmnopqrstuvwxy'
+        letters='abcdefghijklmnopqrstuvwxyz'
         index=[s.index(l) for l in letters if s.count(l) == 1]
         return min(index) if len(index) > 0 else -1
