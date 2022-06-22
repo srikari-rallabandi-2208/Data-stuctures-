f0065f140353e9ad7a23cce0418bd11480353ad6
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-06-22 18:35:50 +0530

    Update bubble_sort.py

--- searchnsort/bubble_sort.py
+++ searchnsort/bubble_sort.py
@@ -9,7 +9,7 @@ def bubbleSort(array):
 		is_sorted = True
 		for i in range(len(array) - 1 - counter):
 			if array[i] > array[i + 1]:
-				swap(i, i + 1, array)
+				swap(i, i , array)
 				is_sorted = False
 		counter += 1
 	return array
