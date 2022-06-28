35ed318cd49bd6eb42c6be196f87de579ab646af
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-06-29 00:07:25 +0530

    Update temparatures.py

--- stacks/temparatures.py
+++ stacks/temparatures.py
@@ -10,6 +10,6 @@ def dailyTemperatures(self, temperatures):
     for i in range(l):
         while stack and temperatures[stack[-1]] < temperatures[i]:
             index = stack.pop()
-            result[index] = i-index
+            result[index] = i-inde
         stack.append(i)
     return result
