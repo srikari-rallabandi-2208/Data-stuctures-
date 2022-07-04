0c2813d4b984643cb91b0ba3b58744bc0b375960
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-07-04 17:38:31 +0530

    Update check_common_elements.py

--- arrays/check_common_elements.py
+++ arrays/check_common_elements.py
@@ -2,7 +2,7 @@ def common_elements(list1, list2):
     p1 = 0
     p2 = 0
     result = []
-    while p1 < len(list1) and p2 < len(list):
+    while p1 < len(list1) and p2 < len(list2):
         if list1[p1] == list2[p2]:
             result.append(list1[p1])
             p1 += 1
