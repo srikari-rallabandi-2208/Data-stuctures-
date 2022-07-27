c8a286c839f96467c1d37505a8fef4e061e29903
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-07-27 15:15:56 +0530

    Add files via upload

---
+++ stacks/largest_rectangle.py
@@ -0,0 +1,16 @@
+'''
+LeetCode problem - 84
+'''
+
+def largestRectangleArea(self, height):
+    height.append(0)
+    stack = [-1]
+    ans = 0
+    for i in xrange(len(height)):
+        while height[i] < height[stack[-1]]:
+            h = height[stack.pop()]
+            w = i - stack[-1] - 1
+            ans = max(ans, h * w)
+        stack.append(i)
+    height.pop()
+    return ans
