ad033ce7be52954ec78ec383af7bc988298065c3
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-07-30 15:37:05 +0530

    Add files via upload

---
+++ queues/flatten_iterator.py
@@ -0,0 +1,20 @@
+'''
+LeetCode problem - 341
+'''
+
+class NestedIterator(object):
+
+    def __init__(self, nestedList):
+        self.stack = nestedList[::-1]
+        
+    def next(self):
+        return self.stack.pop().getInteger()
+        
+    def hasNext(self):
+        while self.stack:
+            top = self.stack[-1]
+            if top.isInteger():
+                return True
+            self.stack = self.stack[:-1] + top.getList()[::-1]
+        return False
+
