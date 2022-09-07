2485f419c3564507ddde9d86630e375aaa022aa8
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-09-07 14:48:49 +0530

    Add files via upload

---
+++ trees/max_depth.py
@@ -0,0 +1,10 @@
+'''
+LeetCode - problem 104
+'''
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(root, depth):
+            if not root: return depth
+            return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
+                       
+        return dfs(root, 0)
