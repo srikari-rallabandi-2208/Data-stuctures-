c9304e8fbc2892dde9d645dfcf972acfdf2af459
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-09-17 22:40:26 +0530

    Add files via upload

---
+++ miscellaneous/word_break.py
@@ -0,0 +1,9 @@
+'''
+LeetCode - problem 139
+'''
+
+def wordBreak(self, s, words):
+    ok = [True]
+    for i in range(1, len(s)+1):
+        ok += any(ok[j] and s[j:i] in words for j in range(i))
+    return ok[-1]
