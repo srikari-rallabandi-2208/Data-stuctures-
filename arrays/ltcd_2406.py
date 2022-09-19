d147880488ac023a58188ed6ee325fbb2eb24ff5
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-09-19 22:18:18 +0530

    Add files via upload

---
+++ arrays/ltcd_2406.py
@@ -0,0 +1,10 @@
+'''
+LeetCode - problem 2406
+'''
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        pq = []
+        for left, right in sorted(intervals):
+            if pq and pq[0] < left:
+                heappop(pq)
+            heappush(pq, right)
+        return len(pq)
