9534f27cad6395fcba365ef9994d9ec77b106ff8
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-09-25 22:18:03 +0530

    Add files via upload

---
+++ miscellaneous/ltcd_2405.py
@@ -0,0 +1,16 @@
+'''
+LeetCode - problem 2405
+'''
+
+    def partitionString(self, s: str) -> int:
+        cur = set()
+        res = 1
+        
+        for c in s:
+            if c in cur:
+                cur = set()
+                res += 1
+            cur.add(c)
+                
+        return res
+
