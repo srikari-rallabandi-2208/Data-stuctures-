019b9ecbbd2d4a18a1fb89900765eec92a1f7363
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2022-10-26 18:01:03 +0530

    Add files via upload

---
+++ arrays/ltcd_128.py
@@ -0,0 +1,28 @@
+'''
+LeetCode - problem 128
+'''
+
+def longestConsecutive(self, nums: List[int]) -> int:
+	best: int = 0
+	x: Dict[int, int] = {i: 1 for i in nums}
+
+	for i in list(x.keys()):
+		if i not in x:
+			continue
+		counter: int = 1
+		j: int = 1
+		while i + j in x:
+			counter += 1
+			del x[i + j]
+			j += 1
+		j = 1
+		while i - j in x:
+			counter += 1
+			del x[i - j]
+			j += 1
+		del x[i]
+
+		best = max(best, counter)
+		if not x:
+			break
+	return best
