dcb0dfa46052a833f1a2c0d1a806c3c424556179
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2023-06-04 22:45:05 +0530

    Add files via upload

---
+++ arrays/remove_dup_sorted.py
@@ -0,0 +1,15 @@
+'''
+Leetcode - problem 26
+'''
+
+def removeDuplicates(self, nums: List[int]) -> int:
+	duplicates = 0
+
+	for i in range(1, len(nums)):
+		if nums[i] == nums[i - 1]:
+			duplicates += 1
+		else:
+			nums[i - duplicates] = nums[i]
+
+	return len(nums) - duplicates
+
