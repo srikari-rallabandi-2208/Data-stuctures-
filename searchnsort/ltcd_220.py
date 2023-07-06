729196db9c39bab0c5c9bd961dc211e0b6ed6695
Srikari Rallabandi <55296296+srikari-rallabandi-2208@users.noreply.github.com>
2023-07-06 15:12:54 +0530

    Add files via upload

---
+++ searchnsort/ltcd_220.py
@@ -0,0 +1,15 @@
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        d = OrderedDict()
+        window = t + 1
+        for i, n in enumerate(nums):
+            while len(d) > k:
+                d.popitem(last=False)
+            if (b := n//window) in d:
+                return True 
+            if b - 1 in d and abs(d[b-1] - n) <= t:
+                return True
+            if b + 1 in d and abs(d[b+1] - n) <= t:
+                return True
+            d[b] = nums[i]  
+        return False 
+
